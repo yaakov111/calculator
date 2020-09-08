e4c7f788e5f1ea3f36e2d7f2d43c6ab67fd7ac22
yaakov111 <52676205+yaakov111@users.noreply.github.com>
2020-09-08 16:45:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var dif3;
 var days;
 var rezo;
 var www=1;
-
+var m=1;
 
 
 
@@ -39,13 +39,14 @@ document.getElementById("t").value = "";
 document.getElementById("3").style.display = 'inline-block';
  document.getElementById("4").style.display = 'inline-block';
    days = 0;
-   www=0;
+   www=1;
     document.getElementById("exp").innerHTML = "הזיכוי המגיע ללקוח במקרה של חיוב ללא חבילה מספר ימים הפחות מחודש ויש לזכות אותו בסכום ההפרשים";
 }
 
 function date(selct){
   if(selct==28){
     days = 28;
+    m=1;
   document.getElementById(selct).style.background = '#FAFAD2';
   document.getElementById("29").style.background = '#ffffff';
   document.getElementById("30").style.background = '#ffffff';
@@ -53,6 +54,7 @@ function date(selct){
 }
 if(selct==29){
     days = 29;
+    m=1;
 document.getElementById(selct).style.background = '#FAFAD2';
 document.getElementById("28").style.background = '#ffffff';
 document.getElementById("30").style.background = '#ffffff';
@@ -60,6 +62,7 @@ document.getElementById("31").style.background = '#ffffff';
 }
 if(selct==30){
     days = 30;
+    m=1;
 document.getElementById(selct).style.background = '#FAFAD2';
 document.getElementById("29").style.background = '#ffffff';
 document.getElementById("28").style.background = '#ffffff';
@@ -67,6 +70,7 @@ document.getElementById("31").style.background = '#ffffff';
 }
 if(selct==31){
     days = 31;
+    m=1;
 document.getElementById(selct).style.background = '#FAFAD2';
 document.getElementById("29").style.background = '#ffffff';
 document.getElementById("30").style.background = '#ffffff';
@@ -75,17 +79,31 @@ document.getElementById("28").style.background = '#ffffff';
 }
 
 function cal() {
- if(www==0){
+
+  if (m==0) {
+    alert("חובה לבחור את מספר הימים בחודש");
+  }else if (document.getElementById("t").value==""||document.getElementById("u").value==""||(document.getElementById("4").value==""&&www==1)) {
+    alert("חובה למלאות את כל השדות");
+  }else if(www==0 && document.getElementById("u").value > days){
+alert("מספר הימים שבעבורם שולם לא יכול להיות גדול ממספר הימים בחודש");
+  }
+  else if(www==1 && document.getElementById("4").value > days){
+alert("מספר הימים שבעבורם שולם לא יכול להיות גדול ממספר הימים בחודש");
+  }
+
+  else if(www==0){
     full1 = document.getElementById("t").value;
     full2 = document.getElementById("u").value;
     rezo = (full1/days)*full2;
-             rez.innerHTML= rezo;
+  rezo = rezo.toFixed(2);
+             rez.innerHTML=" הזיכוי המגיע ללקוח הוא" +" "+ rezo;
   }else if (www==1) {
     dif1 = document.getElementById("t").value;
     dif2 = document.getElementById("u").value;
     dif3 = document.getElementById("4").value;
     rezo = ((dif2/days)*dif3)-((dif1/days)*dif3)
-     rez.innerHTML= rezo;
+ rezo = rezo.toFixed(2);
+     rez.innerHTML=" הזיכוי המגיע ללקוח בגין הימים שבהם חויב בתעריף הגבוה הוא" +" "+  rezo;
   }
 
 }
